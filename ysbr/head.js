be86c03d62171c9d6ef5db92b099511548641c1b
Lucabudi <39259808+Lucabudi@users.noreply.github.com>
2022-05-23 19:31:52 +0200

    Delete ysbr directory

--- ysbr/head.js
+++
@@ -1,8 +0,0 @@
-fetch('head.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#replace_with_head");
-    let newelem = document.createElement("head");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
